088fd30f20277f12d30efaa01224187c8d196393
ginftls <scdr1905@gmail.com>
2024-11-27 08:25:12 -0500

    ptintf01

--- test.c
+++ test.c
@@ -1,17 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdarg.h>
-#include <unistd.h>
-
-/* Function prototypes */
-int _printf(const char *format, ...);
-int print_number(int n);
-int _putchar(char c);
-
-#endif /* MAIN_H */
-
-
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
