c9f448a42afc855234d80364cef210fd20949a39
ginftls <scdr1905@gmail.com>
2024-11-27 07:04:37 -0500

    printf-01

--- main.h
+++ main.h
@@ -4,8 +4,9 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/* Function prototypes */
 int _printf(const char *format, ...);
+int print_number(int n);
 int _putchar(char c);
 
 #endif /* MAIN_H */
-
